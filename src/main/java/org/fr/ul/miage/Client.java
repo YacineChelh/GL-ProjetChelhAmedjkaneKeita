35f3bfa8f05a9c6bbb656f1f6e7ef9dae80574cd
Yacine <yacine.chelh@gmail.com>
2024-05-27 18:41:56 +0200

    Modification de la structure de la classe Client + Implémentation des tests pour la classe Client
    Modification de la classe Client avec l'ajout de constructeur + Ajout d'une fonction MenuPrincipal dans le Main + Ajout des quelques fonctions dans la classe Database
    
    Modification de la classe Client avec l'ajout de constructeur + Ajout d'une fonction MenuPrincipal dans le Main + Ajout des quelques fonctions dans la classe Database + Connexion à la base de données
    
    Modification de la classe Client avec l'ajout de constructeur + Ajout d'une fonction MenuPrincipal dans le Main + Ajout des quelques fonctions dans la classe Database + Connexion à la base de données
    
    Modification de la classe Client avec l'ajout de constructeur + Ajout d'une fonction MenuPrincipal dans le Main + Ajout des quelques fonctions dans la classe Database + Connexion à la base de données
    
    Modification de la classe Client avec l'ajout de constructeur + Ajout d'une fonction MenuPrincipal dans le Main + Ajout des quelques fonctions dans la classe Database + Connexion à la base de données

--- src/main/java/org/fr/ul/miage/Client.java
+++ src/main/java/org/fr/ul/miage/Client.java
@@ -1,10 +1,12 @@
 package org.fr.ul.miage;
 
+import java.sql.Timestamp;
+import java.util.List;
 import java.util.Scanner;
 
 public class Client {
-    Scanner scanner = new Scanner(System.in);
-    private Database database = new Database();
+    static Scanner scanner = new Scanner(System.in);
+    private static Database database = new Database();
     String nom;
     String prenom;
     String mail;
@@ -13,163 +15,147 @@ public class Client {
     String numCarteCredit;
     String immatriculation;
 
-
-    //Inscription
+    // Constructeur
     public Client() {
+    }
+
+    public Client(String nom, String prenom, String mail, String adresse, String telephone, String numCarteCredit) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.mail = mail;
+        this.adresse = adresse;
+        this.telephone=telephone;
+        this.numCarteCredit=numCarteCredit;
+
+    }
+
+    public Client(String nom, String prenom, String mail, String adresse, String telephone, String numCarteCredit, String immatriculation) {
+        if(immatriculation != null) {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.mail = mail;
+            this.adresse = adresse;
+            this.telephone = telephone;
+            this.numCarteCredit = numCarteCredit;
+            this.immatriculation = immatriculation;
+        }else{
+            new Client(nom,prenom,mail,adresse,telephone,numCarteCredit);
+        }
+    }
+
+    // Méthode d'inscription
+    public static void inscription() {
+        String nom;
+        String prenom;
+        String mail;
+        String adresse;
+        String telephone;
+        String numCarteCredit;
+        String immatriculation = null;
         System.out.println("Bonjour et bienvenue sur la page inscription. \nQuel est votre nom ?");
         String temp = scanner.next();
-        this.nom = verifInput(temp);
+        while (!verifNomPrenom(temp)) {
+            System.out.println("Nom invalide. Quel est votre nom ?");
+            temp = scanner.next();
+        }
+        nom = temp;
 
         System.out.println("Ensuite, quel est votre prénom ?");
         temp = scanner.next();
-        this.prenom = verifInput(temp);
+        while (!verifNomPrenom(temp)) {
+            System.out.println("Prénom invalide. Quel est votre prénom ?");
+            temp = scanner.next();
+        }
+        prenom = temp;
 
         System.out.println("Quel est votre adresse ?");
-        temp= scanner.next();
-        this.adresse = verifAdresse(temp);
+        temp = scanner.next();
+        while (!verifAdresse(temp)) {
+            System.out.println("Adresse invalide. Quel est votre adresse ?");
+            temp = scanner.next();
+        }
+        adresse = temp;
 
         System.out.println("Quel est votre numéro de téléphone ?");
-        temp= scanner.next();
-        this.telephone = verifTel(temp);
+        temp = scanner.next();
+        while (!verifTel(temp)) {
+            System.out.println("Numéro de téléphone invalide. Quel est votre numéro de téléphone ?");
+            temp = scanner.next();
+        }
+        telephone = temp;
 
         System.out.println("Quelle est votre adresse mail ?");
-        temp=scanner.next();
-        this.mail = verifMail(temp);
+        temp = scanner.next();
+        while (!verifMail(temp)) {
+            System.out.println("Adresse mail invalide. Quelle est votre adresse mail ?");
+            temp = scanner.next();
+        }
+        mail = temp;
 
         System.out.println("Quel est votre numéro de carte de crédit ?");
-        temp=scanner.next();
-        this.numCarteCredit=verifCarte(temp);
+        temp = scanner.next();
+        while (!verifCarte(temp)) {
+            System.out.println("Numéro de carte de crédit invalide. Quel est votre numéro de carte de crédit ?");
+            temp = scanner.next();
+        }
+        numCarteCredit = temp;
 
         System.out.println("Avez-vous un véhicule ? O/N");
-        if(verifReponse(scanner.next())=="O"){
-            System.out.println("Veuillez l'immatriculation de votre véhicule :");
-            temp=scanner.next();
-            this.immatriculation=verifImma(temp);
+        String reponse = scanner.next();
+        while (!verifReponse(reponse)) {
+            System.out.println("Veuillez faire un choix cohérent (O/N) :");
+            reponse = scanner.next();
         }
 
-        //Insertion dans la BDD
+        if (reponse.equalsIgnoreCase("O")) {
+            System.out.println("Veuillez entrer l'immatriculation de votre véhicule : (ex : AB-123-CD)");
+            temp = scanner.next();
+            while (!verifImma(temp)) {
+                System.out.println("Immatriculation invalide. Veuillez entrer l'immatriculation de votre véhicule (ex : AB-123-CD) :");
+                temp = scanner.next();
+            }
+            immatriculation = temp;
+        }
+        new Client(nom,prenom,mail,adresse,telephone,numCarteCredit,immatriculation);
+        // Insertion dans la BDD
         database.insertClient(nom, prenom, adresse, telephone, mail, numCarteCredit, immatriculation);
-        System.out.println("Très bien. Inscription terminée. Bienvenue "+this.prenom);
+        System.out.println("Très bien. Inscription terminée. Bienvenue " + prenom);
+    }
+
+    public static boolean verifReponse(String verif) {
+        return verif.equalsIgnoreCase("O") || verif.equalsIgnoreCase("N");
+    }
+
+    public static boolean verifNomPrenom(String verif) {
+        return verif.length() > 0 && verif.matches("[a-zA-Z]+(\\s[a-zA-Z]+)*");
+    }
+
+    public static boolean verifAdresse(String verif) {
+        return verif.length() >= 5 && verif.length() <= 100 && verif.matches("[\\w\\s\\d.,'-]+");
+    }
+
+    public static boolean verifTel(String verif) {
+        return verif.matches("^(0)[0-9]{9}$");
+    }
+
+    public static boolean verifMail(String verif) {
+        return verif.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]+$");
+    }
+
+    public static boolean verifCarte(String verif) {
+        return verif.length() == 16 && verif.matches("[0-9]+");
+    }
+
+    public static boolean verifImma(String verif) {
+        return verif.matches("[A-Z]{2}-[0-9]{3}-[A-Z]{2}");
     }
 
 
-
-    public String verifReponse(String verif) {
-        while (true) {
-            if (verif.equalsIgnoreCase("O")) {
-                return "O";
-            } else if (verif.equalsIgnoreCase("N")) {
-                return "N";
-            } else {
-                System.out.println("Veuillez entrer une réponse valide (O/N) :");
-                verif = scanner.nextLine();
-            }
+    public void verificationBorneDisponible(Timestamp debut, Timestamp fin){
+        List<Integer> listeBorneDisponible = database.getToutesLesBornesDisponibles(debut,fin);
+        System.out.println("Liste des bornes disponibles : ");
+        for(int i = 0; i<listeBorneDisponible.size();i++){
+            System.out.println("- Borne "+(listeBorneDisponible.get(i)));
         }
     }
-
-    public String verifInput(String verif) {
-        boolean keepGoing = true;
-
-        while (keepGoing) {
-            if (verif.length() == 0) {
-                System.out.println("Veuillez entrer un champ valide !");
-                verif = scanner.next();
-            } else if (!verif.matches("[a-zA-Z]+")) {
-                System.out.println("Le nom ne doit contenir que des lettres !\nVeuillez la saisir à nouveau :");
-                verif = scanner.next();
-            } else {
-                keepGoing = false;
-            }
-        }
-        return verif;
-    }
-
-    public String verifAdresse(String verif) {
-        boolean keepGoing = true;
-
-        while (keepGoing) {
-            if (verif.length() == 0) {
-                System.out.println("Veuillez entrer une adresse valide !");
-                verif = scanner.nextLine();
-            } else if (verif.length() < 5 || verif.length() > 100) {
-                System.out.println("L'adresse doit contenir entre 5 et 100 caractères ! Veuillez la saisir à nouveau :");
-                verif = scanner.nextLine();
-            } else if (!verif.matches("[\\w\\s\\d.,'-]+")) {
-                System.out.println("L'adresse contient des caractères invalides !\n Veuillez la saisir à nouveau :");
-                verif = scanner.nextLine();
-            } else {
-                keepGoing = false;
-            }
-        }
-        return verif;
-    }
-
-    public String verifTel(String verif) {
-        boolean keepGoing = true;
-
-        while (keepGoing) {
-            if (verif.length() == 0) {
-                System.out.println("Veuillez entrer un numéro de téléphone valide !\n Veuillez le saisir à nouveau :");
-                verif = scanner.next();
-            } else if (!verif.matches("^(06|07)[0-9]{8}$")) { //Regex qui vérifie la structure de le telephone entré
-                System.out.println("Le numéro de téléphone doit commencer par 0 et contenir exactement 10 chiffres ! \n Veuillez entrer à nouveau un numéro valide :");
-                verif = scanner.next();
-            } else {
-                keepGoing = false;
-            }
-        }
-        return verif;
-    }
-
-    public String verifMail(String verif) {
-        boolean keepGoing = true;
-
-        while (keepGoing) {
-            if (verif.length() == 0) {
-                System.out.println("Veuillez entrer une adresse e-mail valide ! \nVeuillez entrer une adresse valide :");
-                verif = scanner.next();
-            } else if (!verif.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) { //Regex qui vérifie la structure de l'adresse mail entré
-                System.out.println("L'adresse e-mail n'est pas valide ! \nVeuillez entrer une adresse valide :");
-                verif = scanner.next();
-            } else {
-                keepGoing = false;
-            }
-        }
-        return verif;
-    }
-
-    public String verifCarte(String verif) {
-        boolean keepGoing = true;
-
-        while(keepGoing) {
-            if (verif.length() != 16) {
-                System.out.println("Le numéro de carte de crédit doit contenir exactement 16 chiffres. \nVeuillez le saisir à nouveau : ");
-                verif = scanner.nextLine();
-            } else if (!verif.matches("[0-9]+")) {
-                System.out.println("Le numéro de carte de crédit ne doit contenir que des chiffres. \nVeuillez le saisir à nouveau :");
-                verif = scanner.nextLine();
-            } else {
-                keepGoing = false;
-            }
-        }
-        return verif;
-    }
-
-
-    public String verifImma(String verif){
-        boolean keepGoing = true;
-
-        while(keepGoing) {
-            if (verif.matches("[A-Z]{2}-[0-9]{3}-[A-Z]{2}")) {
-                return verif;
-            } else {
-                System.out.println("Veuillez entrer une immatriculation valide (ex: AB-123-CD) :");
-                verif = scanner.nextLine();
-            }
-        }
-
-        return verif;
-    }
-
 }
-
--- src/main/java/org/fr/ul/miage/Main.java
+++ src/main/java/org/fr/ul/miage/Main.java
@@ -6,26 +6,128 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     static Scanner scanner = new Scanner (System.in);
+    static Database database = new Database();
+    static Client client = new Client();
     public static void main(String[] args) {
-
-        Client c = new Client();
+        client.inscription();
+        menuPrincipal();
     }
 
-    public static String verifTel(String verif) {
+    public static void menuPrincipal() {
+        int choix = 0;
         boolean keepGoing = true;
+        int numRes;
+        String numImma;
+        String numTel;
 
-        while (keepGoing) {
-            if (verif.length() == 0) {
-                System.out.println("Veuillez entrer un numéro de téléphone valide !");
-                verif = scanner.next();
-            } else if (!verif.matches("^(06|07)[0-9]{8}$")) { //Regex qui vérifie la structure de le telephone entré
-                System.out.println("Le numéro de téléphone doit commencer par 0 et contenir exactement 10 chiffres ! \n Veuillez entrer à nouveau un numéro valide :");
-                verif = scanner.next();
-            } else {
-                keepGoing = false;
+        while(keepGoing) {
+
+            System.out.println("Bienvenue sur le menu principal de CarBorne. Que souhaitez-vous faire ?");
+            System.out.println("1. Effectuer une réservation de borne");
+            System.out.println("2. Consulter les réservations");
+            System.out.println("3. Modifier une réservation");
+            System.out.println("4. Prolonger une réservation");
+            System.out.println("5. Consulter la disponibilité des bornes");
+            System.out.println("6. Quitter");
+
+            System.out.print("Entrez votre choix : ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Erreur : Veuillez saisir un choix valide.");
+                scanner.next();
             }
+            choix = scanner.nextInt();
+
+            switch (choix) {
+                case 1:
+                    System.out.println("Vous avez choisi d'effectuer une réservation.");
+                    System.out.println("Souhaitez-vous entrer votre numéro de réservation (1) ou votre numéro d'immatriculation (2) ?");
+                    while (!scanner.hasNextInt()) {
+                        System.out.println("Erreur : Veuillez saisir un choix valide.");
+                        scanner.next();
+                    }
+
+                    int choixReservation = scanner.nextInt();
+
+                    switch (choixReservation) {
+                        case 1:
+                            System.out.println("Vous avez choisi de saisir votre numéro de réservation. Veuillez saisir votre numéro de réservation : ");
+                            while (!scanner.hasNextInt()) {
+                                System.out.println("Erreur : Veuillez saisir un numéro de réservation valide.");
+                                scanner.next();
+                            }
+                            numRes = scanner.nextInt();
+                            System.out.println("Verification du numéro de réservation : " + numRes);
+                            //Appel de la fonction qui vérifie si la réservation existe et qui correspond à ce créneau (10min avant le rendez-vous au moins) Database.existeReservation()
+                            break;
+                        case 2:
+                            System.out.println("Vous avez choisi de saisir votre numéro d'immatriculation. (Exemple : AB-123-CD)");
+                            while (!Client.verifImma(scanner.next())) {
+                                System.out.println("Erreur : Veuillez saisir un numéro d'immatriculation valide. (Exemple : AB-123-CD)");
+                                scanner.next();
+                            }
+                            numImma = scanner.next();
+                            System.out.println("Verification du numéro d'immatriculation : " + numImma);
+                            if (database.existingImmatriculation(numImma)) {
+                                System.out.println("L'immatriculation saisie existe bien dans la base de données.");
+                                //TODO : Suite du process de recharge apres imma
+                            } else {
+                                System.out.println("L'immatriculation saisie n'existe pas. Veuillez entrer un numéro de téléphone :");
+                                while (!Client.verifTel(scanner.next())) {
+                                    System.out.println("Erreur : Veuillez saisir un numéro de téléphone valide.");
+                                    scanner.next();
+                                }
+                                numTel = scanner.next();
+                                System.out.println("Verification du numéro de téléphone : " + numTel);
+                                if (database.existingImmatriculation(numTel)) {
+                                    System.out.println("Le numéro de téléphone saisie existe bien dans la base de données.");
+                                    //TODO : Suite du processus de recharge apres tel
+                                } else {
+                                    System.out.println("Le numéro de téléphone saisie n'existe pas. Voulez-vous inscrire pour pouvoir continuer ?\n 1. OUI\n 2. NON");
+
+                                    while (!scanner.hasNextInt()) {
+                                        System.out.println("Erreur : Veuillez saisir un choix valide.");
+                                        scanner.next();
+                                    }
+
+                                    int choixInscription = scanner.nextInt();
+                                    switch (choixInscription) {
+                                        case 1:
+                                            Client.inscription();
+                                            break;
+                                        case 2:
+                                            System.out.println("Vous avez choisi de ne pas vous inscrire...");
+                                            break;
+                                        default:
+                                            System.out.println("Veuillez faire un choix valide");
+                                    }
+                                }
+                            }
+                            break;
+                        default:
+                            System.out.println("Choix invalide. Veuillez saisir 1 ou 2.");
+                    }
+                    break;
+                case 2:
+                    System.out.println("Vous avez choisi de consulter les réservations.");
+                    break;
+                case 3:
+                    System.out.println("Vous avez choisi de modifier une réservation.");
+                    break;
+                case 4:
+                    System.out.println("Vous avez choisi de prolonger une réservation.");
+                    break;
+                case 5:
+                    System.out.println("Vous avez choisi de consulter la disponibilité des bornes.");
+                    break;
+                case 6:
+                    System.out.println("Vous avez choisi de quitter. Au revoir !");
+                    keepGoing=false;
+                    break;
+                default:
+                    System.out.println("Choix invalide. Veuillez saisir un choix valide.");
+            }
+
+
         }
-        System.out.println("Ok c bon");
-        return verif;
     }
 }
